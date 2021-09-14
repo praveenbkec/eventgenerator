3de8c28b961a8c69f7a03b9a557119e2da291508
kmpraveen <kmpraveen@juniper.net>
2021-09-15 01:21:31 +0530

    remove commeneted code

--- consumer/main.go
+++ consumer/main.go
@@ -48,10 +48,9 @@ func RunGrpcGateway(address string, opts ...runtime.ServeMuxOption) error {
 		return err
 	}
 	mux.Handle("/", gw)
-	//return http.ListenAndServe(address, allowCORS(mux))
 	return http.ListenAndServe(address, mux)
 }
-//TODO Login interceptor example
+//TODO Login/Log interceptor example
 func newGateway(ctx context.Context, opts ...runtime.ServeMuxOption) (http.Handler, error) {
 	fmt.Println("********* Creating newGateway ************")
 	mux := runtime.NewServeMux(opts...)
@@ -71,30 +70,5 @@ func newGateway(ctx context.Context, opts ...runtime.ServeMuxOption) (http.Handl
 	return mux, nil
 }
 
-//TODO check whether required
-//allowCORS allows Cross Origin Resoruce Sharing from any origin.
-//Don't do this without consideration in production systems.
-//func allowCORS(h http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		if origin := r.Header.Get("Origin"); origin != "" {
-//			w.Header().Set("Access-Control-Allow-Origin", origin)
-//			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
-//				preflightHandler(w, r)
-//				return
-//			}
-//		}
-//		h.ServeHTTP(w, r)
-//	})
-//}
-
-//func preflightHandler(w http.ResponseWriter, r *http.Request) {
-//	headers := []string{"Content-Type", "Accept"}
-//	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
-//	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
-//	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
-//	//glog.Infof("preflight request for %s", r.URL.Path)
-//	fmt.Println("preflight request for %s", r.URL.Path)
-//	return
-//}
 
 
